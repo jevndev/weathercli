8a6e41c630fab0b035c774a7f7b020c0933f1162
Jonathan Evans <JonathanEvansDev@gmail.com>
2024-06-16 13:35:38 -0500

    Add geocode API call

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/spf13/pflag"
@@ -52,6 +54,42 @@ func getCommandLineArguments() (arguments programArguments, err error) {
 	return parsedArguments, nil
 }
 
+func requestGeoCode(locationName string, apiKey string) (resp *http.Response, err error) {
+	return http.Get(fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s", locationName, apiKey))
+}
+
+type LatLon struct {
+	Latitude  float64
+	Longitude float64
+}
+
+type GeoCodeResponseItem struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
+}
+
+func requestLatLonOfLocation(locationName string, apiKey string) (latlon LatLon, err error) {
+	apiResponse, err := requestGeoCode(locationName, apiKey)
+	if err != nil {
+		return LatLon{}, err
+	}
+	defer apiResponse.Body.Close()
+
+	var responseItems []GeoCodeResponseItem
+
+	jsonDecodeErr := json.NewDecoder(apiResponse.Body).Decode(&responseItems)
+
+	if jsonDecodeErr != nil {
+		return LatLon{}, jsonDecodeErr
+	}
+
+	if len(responseItems) != 1 {
+		return LatLon{}, errors.New("got an unexpected number of returned locations from openstreetmap")
+	}
+
+	return LatLon{}, nil
+}
+
 func main() {
 	arguments, err := getCommandLineArguments()
 	if err != nil {
@@ -61,5 +99,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(arguments.apiKey, arguments.location)
+	locationLatLon, latLonRequestError := requestLatLonOfLocation(arguments.location, arguments.apiKey)
+
+	if latLonRequestError != nil {
+		fmt.Printf("\033[0;31m%v\033[0m\n\n", err.Error())
+		os.Exit(1)
+	}
 }
